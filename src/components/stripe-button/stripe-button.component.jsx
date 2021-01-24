92b32473058c103f91f6001ad4fdedce378ce694
patelsmit11 <patel.smit11@gmail.com>
2021-01-24 01:52:05 -0500

    Updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = "pk_test_51ICv0yL4bFMwwVFwVVBh4dPhWa5vgj0i7YxatJ6PwgEzmZqLhvwgWwi0XHt75g5xmalYFNFgmCQoIBQxBrX0fbCn00uSdbSGc1";
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');  
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress 
+            shippingAddress
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton;
